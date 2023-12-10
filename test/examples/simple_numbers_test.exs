750a86fe9efb9b89cb1aef40eae53be282352719
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2023-12-10 16:43:38 +0100

    RoundRobin mixer example

--- test/examples/simple_numbers_test.exs
+++ test/examples/simple_numbers_test.exs
@@ -25,4 +25,57 @@ defmodule Strom.Examples.SimpleNumbersTest do
     assert Enum.sort(Enum.to_list(odd)) == [3, 5, 7, 9, 11]
     assert Enum.sort(Enum.to_list(even)) == [2, 4, 6, 8, 10]
   end
+
+  describe "round robin mixer" do
+    defmodule RoundRobin do
+      use Strom.DSL
+
+      def add_label(event, label), do: {event, label}
+
+      defmodule DoMix do
+        def start(_opts), do: %{first: [], second: []}
+
+        def call({number, :first}, acc, _opts) do
+          case acc[:second] do
+            [hd | tl] ->
+              {[hd, number], Map.put(acc, :second, tl)}
+
+            [] ->
+              firsts = Map.fetch!(acc, :first)
+              {[], Map.put(acc, :first, firsts ++ [number])}
+          end
+        end
+
+        def call({number, :second}, acc, _opts) do
+          case acc[:first] do
+            [hd | tl] ->
+              {[hd, number], Map.put(acc, :first, tl)}
+
+            [] ->
+              seconds = Map.fetch!(acc, :second)
+              {[], Map.put(acc, :second, seconds ++ [number])}
+          end
+        end
+
+        def stop(_acc, _opts), do: :ok
+      end
+
+      @topology [
+        function(:first, &__MODULE__.add_label/2, :first),
+        function(:second, &__MODULE__.add_label/2, :second),
+        mixer([:first, :second], :mixed),
+        module(:mixed, DoMix)
+      ]
+    end
+
+    test "test the order of numbers" do
+      RoundRobin.start()
+
+      %{mixed: mixed} =
+        %{first: [1, 2, 3], second: [10, 20, 30]}
+        |> RoundRobin.call()
+
+      assert Enum.to_list(mixed) == [1, 10, 2, 20, 3, 30]
+    end
+  end
 end
