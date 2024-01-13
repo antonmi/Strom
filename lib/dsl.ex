901041a40fc260daae87c25d2aec5626aa04ae90
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2024-01-13 21:23:23 +0100

    Refactor DSL

--- lib/dsl.ex
+++ lib/dsl.ex
@@ -1,5 +1,4 @@
 defmodule Strom.DSL do
-
   alias Strom.{Transformer, Mixer, Renamer, Sink, Source, Splitter}
 
   defmacro source(name, origin) do
@@ -16,52 +15,25 @@ defmodule Strom.DSL do
 
   defmacro mix(inputs, output, opts \\ []) do
     quote do
-      %Mixer{inputs: unquote(inputs), output: unquote(output), opts: unquote(opts)}
+      %{Mixer.new(unquote(inputs), unquote(output)) | opts: unquote(opts)}
     end
   end
 
   defmacro split(input, outputs, opts \\ []) do
     quote do
-      unless is_map(unquote(outputs)) and map_size(unquote(outputs)) > 0 do
-        raise "Outputs in splitter must be a map, given: #{inspect(unquote(outputs))}"
-      end
-
-      %Splitter{
-        input: unquote(input),
-        outputs: unquote(outputs),
-        opts: unquote(opts)
-      }
+      %{Splitter.new(unquote(input), unquote(outputs)) | opts: unquote(opts)}
     end
   end
 
   defmacro transform(names, function, acc \\ nil, opts \\ []) do
     quote do
-      %Transformer{
-        function: unquote(function),
-        acc: unquote(acc),
-        opts: unquote(opts),
-        names: unquote(names)
-      }
-    end
-  end
-
-  defmacro from(module, opts \\ []) do
-    quote do
-      unless is_atom(unquote(module)) do
-        raise "Flow must be a module, given: #{inspect(unquote(module))}"
-      end
-
-      apply(unquote(module), :topology, [unquote(opts)])
+      %{Transformer.new(unquote(names), unquote(function), unquote(acc)) | opts: unquote(opts)}
     end
   end
 
   defmacro rename(names) do
     quote do
-      unless is_map(unquote(names)) do
-        raise "Names must be a map, given: #{inspect(unquote(names))}"
-      end
-
-      %Renamer{names: unquote(names)}
+      Renamer.new(unquote(names))
     end
   end
 end
