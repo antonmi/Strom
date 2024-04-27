1225ec8c23d192d7fce9f7295b66623337812247
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2024-04-27 17:06:28 +0200

    Set version to 0.8.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Strom.MixProject do
   def project do
     [
       app: :strom,
-      version: "0.8.0",
+      version: "0.8.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       description: description(),
